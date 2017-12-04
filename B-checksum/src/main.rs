23b5c60dd2033af1a32ca393c86e36ed3a1be98e
Ms2ger <Ms2ger@gmail.com>
2017-12-04 11:25:11 +0100

    Day 2: part 2 (implementation).

--- B-checksum/src/main.rs
+++ B-checksum/src/main.rs
@@ -25,15 +25,29 @@ fn checksum(input: &str) -> u32 {
     })
 }
 
+fn divisible_values(input: &str) -> u32 {
+    sum_lines(input, |values| {
+        let values = values.collect::<Vec<_>>();
+        values.iter().filter_map(|&v1| {
+            values.iter().find(|&&v2| v1 != v2 && v2 % v1 == 0).map(|&v2| v2 / v1)
+        }).next()
+    })
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     let result = checksum(&input);
     println!("Checksum: {}", result);
+    let result = divisible_values(&input);
+    println!("Divisible values: {}", result);
 }
 
 #[test]
 fn example() {
     let example = "5 1 9 5\n7 5 3\n2 4 6 8";
     assert_eq!(checksum(example), 18);
+
+    let example = "5 9 2 8\n9 4 7 3\n3 8 6 5";
+    assert_eq!(divisible_values(example), 9);
 }
